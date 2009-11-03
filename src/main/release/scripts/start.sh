5889c5e22486281da13ef752564dfe75fec951a6
ak.101tec <ak.101tec@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-11-03 17:19:32 +0000

    add installer

---
+++ src/main/release/scripts/start.sh
@@ -0,0 +1,162 @@
+#!/bin/sh
+#
+##
+# Environment Variables
+#
+#   INGRID_JAVA_HOME Overrides JAVA_HOME.
+#
+#   INGRID_HEAPSIZE  heap to use in mb, if not setted we use 1000.
+#
+#   INGRID_OPTS      addtional java runtime options
+#
+
+THIS="$0"
+
+# some directories
+THIS_DIR=`dirname "$THIS"`
+INGRID_HOME=`cd "$THIS_DIR" ; pwd`
+PID=$INGRID_HOME/ingrid.pid
+INGRID_OPTS="-Djetty.port=8088"
+
+# functions
+stopIplug()
+{
+  echo "Try stopping jetty ($INGRID_HOME)..."
+  if [ -f $PID ]; then
+      procid=`cat $PID`
+      idcount=`ps -p $procid | wc -l`
+      if [ $idcount -eq 2 ]; then
+        echo stopping $command
+        kill -9 `cat $PID`
+        echo "process ($procid) has been terminated."
+      else
+        echo "process is not running. Exit."
+        exit 1
+      fi
+    else
+      echo "process is not running. Exit."
+      exit 1
+    fi
+}
+
+stopNoExitIplug()
+{
+  echo "Try stopping jetty ($INGRID_HOME)..."
+  if [ -f $PID ]; then
+      procid=`cat $PID`
+      idcount=`ps -p $procid | wc -l`
+      if [ $idcount -eq 2 ]; then
+        echo stopping $command
+        kill -9 `cat $PID`
+        echo "process ($procid) has been terminated."
+      else
+        echo "process is not running. Exit."
+      fi
+    else
+      echo "process is not running. Exit."
+    fi
+}
+
+
+startIplug()
+{
+  echo "Try starting jetty ($INGRID_HOME)..."
+  if [ -f $PID ]; then
+      procid=`cat $PID`
+      idcount=`ps -p $procid | wc -l`
+      if [ $idcount -eq 2 ]; then
+        echo plug running as process `cat $PID`.  Stop it first.
+        exit 1
+      fi
+  fi
+  
+  # some Java parameters
+  if [ "$INGRID_JAVA_HOME" != "" ]; then
+    #echo "run java in $INGRID_JAVA_HOME"
+    JAVA_HOME=$INGRID_JAVA_HOME
+  fi
+  
+  if [ "$JAVA_HOME" = "" ]; then
+    echo "Error: JAVA_HOME is not set."
+    exit 1
+  fi
+  
+  JAVA=$JAVA_HOME/bin/java
+  JAVA_HEAP_MAX=-Xmx128m
+  
+  # check envvars which might override default args
+  if [ "$INGRID_HEAPSIZE" != "" ]; then
+    JAVA_HEAP_MAX="-Xmx""$INGRID_HEAPSIZE""m"
+    echo "run with heapsize $JAVA_HEAP_MAX"
+  fi
+
+  # CLASSPATH initially contains $INGRID_CONF_DIR, or defaults to $INGRID_HOME/conf
+  CLASSPATH=${INGRID_CONF_DIR:=$INGRID_HOME/conf}
+  CLASSPATH=${CLASSPATH}:$JAVA_HOME/lib/tools.jar
+  CLASSPATH=${CLASSPATH}:${INGRID_HOME}
+  
+  # so that filenames w/ spaces are handled correctly in loops below
+  IFS=
+  # add libs to CLASSPATH
+  for f in $INGRID_HOME/lib/*.jar; do
+    CLASSPATH=${CLASSPATH}:$f;
+  done
+  # restore ordinary behaviour
+  unset IFS
+  
+  # cygwin path translation
+  if expr `uname` : 'CYGWIN*' > /dev/null; then
+    CLASSPATH=`cygpath -p -w "$CLASSPATH"`
+  fi
+
+  # run it
+  exec nohup "$INGRID_JAVA_HOME"/bin/java $INGRID_HEAPSIZE $INGRID_OPTS -jar start.jar > console.log &
+  
+  echo "jetty ($INGRID_HOME) started."
+  echo $! > $PID
+}
+
+# make sure the current user has the privilege to execute that script
+if [ "$INGRID_USER" = "" ]; then
+# TODO switch back after testing
+  INGRID_USER="ingrid"
+  INGRID_USER="ak"
+fi
+
+STARTING_USER=`whoami`
+if [ "$STARTING_USER" != "$INGRID_USER" ]; then
+  echo "You must be user '$INGRID_USER' to start that script! Set INGRID_USER in environment to overwrite this."
+  exit 1
+fi 
+
+case "$1" in
+  start)
+    startIplug
+    ;;
+  stop)
+    stopIplug
+    ;;
+  restart)
+    stopNoExitIplug
+    echo "sleep 3 sec ..."
+    sleep 3
+    startIplug
+    ;;
+  status)
+    if [ -f $PID ]; then
+      procid=`cat $PID`
+      idcount=`ps -p $procid | wc -l`
+      if [ $idcount -eq 2 ]; then
+        echo "process ($procid) is running."
+      else
+        echo "process is not running. Exit."
+      fi
+    else
+      echo "process is not running. Exit."
+    fi
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|restart|status}"
+    exit 1
+    ;;
+esac

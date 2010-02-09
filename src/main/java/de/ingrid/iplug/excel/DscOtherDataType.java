9e645d3a842ee16438c46bf1537f62034ee9c1b0
me.101tec <me.101tec@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2010-02-09 14:51:46 +0000

    + adding dsc_other support

--- src/main/java/de/ingrid/iplug/excel/DscOtherDataType.java
+++ src/main/java/de/ingrid/iplug/excel/DscOtherDataType.java
@@ -5,10 +5,9 @@ import org.springframework.stereotype.Service;
 import de.ingrid.admin.object.AbstractDataType;
 
 @Service
-public class ExcelDatatype extends AbstractDataType {
+public class DscOtherDataType extends AbstractDataType {
 
-	public ExcelDatatype() {
-        super(EXCEL, new DscOtherDataType());
-	}
-
+    public DscOtherDataType() {
+        super(DSC_OTHER, true);
+    }
 }

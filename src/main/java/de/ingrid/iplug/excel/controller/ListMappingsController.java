854a946c9429ebb1a78bf5123e7e22f1da31e482
aw <aw@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2010-06-02 16:53:56 +0000

    - add ranking type "score" as default to plugDescription (INGRID21-195)

--- src/main/java/de/ingrid/iplug/excel/controller/ListMappingsController.java
+++ src/main/java/de/ingrid/iplug/excel/controller/ListMappingsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import de.ingrid.admin.command.PlugdescriptionCommandObject;
+import de.ingrid.utils.query.IngridQuery;
 
 /**
  * Controller to list mapping.
@@ -28,6 +29,16 @@ public class ListMappingsController {
     @RequestMapping(value = "/iplug-pages/listMappings.html", method = RequestMethod.GET)
     public String listMappings(@ModelAttribute("plugDescription") final PlugdescriptionCommandObject commandObject,
 			final ModelMap model) {
+        
+        // add ranking if not already done
+        boolean isOff  = commandObject.containsRankingType("off");
+        boolean isDate = commandObject.containsRankingType("date");
+        
+        // clear list before adding
+        if (commandObject.getArrayList(IngridQuery.RANKED) != null )
+            commandObject.getArrayList(IngridQuery.RANKED).clear();
+        commandObject.setRankinTypes(true,  isDate, isOff);
+        
         return "/iplug-pages/listMappings";
 	}
 }

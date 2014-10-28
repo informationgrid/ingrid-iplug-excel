abf30422d3cd2a0f87edc0f78140d3234eec947b
aw <aw@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2014-10-28 10:45:28 +0000

    - small improvement

--- src/main/java/de/ingrid/iplug/excel/Configuration.java
+++ src/main/java/de/ingrid/iplug/excel/Configuration.java
@@ -1,5 +1,6 @@
 package de.ingrid.iplug.excel;
 
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.commons.logging.Log;
@@ -14,78 +15,74 @@ import com.tngtech.configbuilder.annotation.valueextractor.PropertyValue;
 import de.ingrid.admin.IConfig;
 import de.ingrid.admin.command.PlugdescriptionCommandObject;
 
-@PropertiesFiles( {"config"} )
-@PropertyLocations(directories = {"conf"}, fromClassLoader = true)
+@PropertiesFiles({ "config" })
+@PropertyLocations(directories = { "conf" }, fromClassLoader = true)
 public class Configuration implements IConfig {
-    
-    private static Log log = LogFactory.getLog(Configuration.class);
-    
+
+    private static Log log = LogFactory.getLog( Configuration.class );
+
     @PropertyValue("plugdescription.fields")
-    public String fields;
-    
+    @DefaultValue("")
+    public List<String> fields;
+
     @PropertyValue("plugdescription.sheets")
     @DefaultValue("")
     public String sheets;
-    
+
     @PropertyValue("plugdescription.isRecordLoader")
     @DefaultValue("false")
     public boolean recordLoader;
-    
+
     @PropertyValue("plugdescription.ranking")
     public String rankings;
-    
+
     private XStream xstream;
-    
-    @Override
-    public void initialize() {
-    }
 
     @Override
-    public void addPlugdescriptionValues( PlugdescriptionCommandObject pdObject ) {
-    	pdObject.put( "iPlugClass", "de.ingrid.iplug.excel.ExcelPlug");
-        
-    	if(pdObject.getFields().length == 0){
-        	if(fields != null){
-        		String[] fieldsList = fields.split(",");
-        		for(String field : fieldsList){
-        			pdObject.addField(field);
-        		}
-        	}
-    	}
-        
-        pdObject.setRecordLoader(recordLoader);
-        if(pdObject.getRankingTypes().length == 0){
-        	if(rankings != null){
-        		String[] rankingList = rankings.split(",");
-        		boolean score = false;
-				boolean date = false;
-				boolean notRanked = false;
-				for(String ranking : rankingList){
-        			if(ranking.equals("score")){
-        				score = true;
-        			}else if (ranking.equals("date")) {
-        				date = true;
-					}else if (ranking.equals("notRanked")) {
-						notRanked = true;
-					}
-        		}
-				pdObject.setRankinTypes(score, date, notRanked);
-        	}
-    	}
-        
-        if(pdObject.get("sheets") == null){
-        	if(!sheets.equals("")){
-        		xstream = new XStream();
-            	pdObject.put("sheets", xstream.fromXML(sheets));
-        	}
+    public void initialize() {}
+
+    @Override
+    public void addPlugdescriptionValues(PlugdescriptionCommandObject pdObject) {
+        pdObject.put( "iPlugClass", "de.ingrid.iplug.excel.ExcelPlug" );
+
+        for (String field : fields) {
+            pdObject.remove( field );
+            pdObject.addField( field );
+        }
+
+        pdObject.setRecordLoader( recordLoader );
+        if (pdObject.getRankingTypes().length == 0) {
+            if (rankings != null) {
+                String[] rankingList = rankings.split( "," );
+                boolean score = false;
+                boolean date = false;
+                boolean notRanked = false;
+                for (String ranking : rankingList) {
+                    if (ranking.equals( "score" )) {
+                        score = true;
+                    } else if (ranking.equals( "date" )) {
+                        date = true;
+                    } else if (ranking.equals( "notRanked" )) {
+                        notRanked = true;
+                    }
+                }
+                pdObject.setRankinTypes( score, date, notRanked );
+            }
+        }
+
+        if (pdObject.get( "sheets" ) == null) {
+            if (!sheets.equals( "" )) {
+                xstream = new XStream();
+                pdObject.put( "sheets", xstream.fromXML( sheets ) );
+            }
         }
     }
 
     @Override
-    public void setPropertiesFromPlugdescription( Properties props, PlugdescriptionCommandObject pd ) {
-    	if(pd.get("sheets") != null){
-    		xstream = new XStream();
-        	props.setProperty("plugdescription.sheets", xstream.toXML(pd.get("sheets")));
-    	}
+    public void setPropertiesFromPlugdescription(Properties props, PlugdescriptionCommandObject pd) {
+        if (pd.get( "sheets" ) != null) {
+            xstream = new XStream();
+            props.setProperty( "plugdescription.sheets", xstream.toXML( pd.get( "sheets" ) ) );
+        }
     }
 }

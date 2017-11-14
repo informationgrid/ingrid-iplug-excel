8c5c1873ad98a41ae24cd41ef374372c448fccfd
Benjamin Röhrl <b.roehrl@gmail.com>
2017-11-14 15:27:32 +0100

    - create docker image

--- src/main/release/scripts/start.sh
+++ src/main/release/scripts/start.sh
@@ -183,7 +183,11 @@ startIplug()
   
   CLASS=de.ingrid.iplug.excel.ExcelPlug
 
-  exec nohup "$JAVA" $INGRID_OPTS $CLASS > console.log &
+  if [ "$RUN_DIRECTLY" ]; then
+    exec "$JAVA" $INGRID_OPTS $CLASS
+  else
+    exec nohup "$JAVA" $INGRID_OPTS $CLASS > console.log &
+  fi
 
   echo "jetty ($INGRID_HOME) started."
   echo $! > $PID
